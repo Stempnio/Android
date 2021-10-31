f32c4924efd77f3f98641b5efc81795c3a6b3b5f
Jakub Stępień <jakub.stepien@student.uj.edu.pl>
2021-10-31 10:53:47 +0100

    working calculator

--- calculator/app/src/main/java/pl/edu/uj/calculator/MainActivity.kt
+++ calculator/app/src/main/java/pl/edu/uj/calculator/MainActivity.kt
@@ -5,122 +5,123 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import org.w3c.dom.Text
 
+
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        val addButton = findViewById<Button>(R.id.btAdd)
-
-        addButton.setOnClickListener {
-            typeOfOperation = TypeOfOperation.ADD
-
-            val tv1: TextView = findViewById(R.id.txtViewTypeOfOperation)
-            tv1.text = "+"
-        }
-    }
 
     enum class TypeOfOperation {
         ADD, SUB, MULT, DIV, MODULO, NONE
     }
 
     var typeOfOperation = TypeOfOperation.NONE
-    var a = 0
-    var b = 0
+    var a: Double = 0.0
+    var b: Double = 0.0
 
-//    fun calculate(view: android.view.View) {
-//        setContentView(R.layout.activity_main)
-//        val textViewResult: TextView = findViewById(R.id.textViewResult)
-//        if(typeOfOperation == TypeOfOperation.NONE) {
-//            textViewResult.setText("Choose type of operation! (+, - etc.")
-//            return
-//        }
-//        if(b == 0 && typeOfOperation == TypeOfOperation.DIV) {
-//            textViewResult.setText("You can't divide by zero!!")
-//            return
-//        }
-//
-//        var result: String = ""
-//        result += a
-//        var resultNumber = 0
-//        when(typeOfOperation) {
-//            TypeOfOperation.ADD ->  {resultNumber = a+b; result += " + "}
-//            TypeOfOperation.SUB -> {resultNumber = a-b; result += " - "}
-//            TypeOfOperation.DIV -> {resultNumber = a/b; result += " / "}
-//            TypeOfOperation.MULT -> {resultNumber = a*b; result += " x "}
-//            TypeOfOperation.MODULO -> {resultNumber = a%b; result += " % "}
-//            else -> System.err.println("unspecified operation")
-//        }
-//        result += b
-//        result += " = "
-//        result += resultNumber
-//
-//        textViewResult.text = result
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    fun adddd() {
+        val txtViewOperation: TextView = findViewById(R.id.txtViewTypeOfOperation)
+
+        val addButton = findViewById<Button>(R.id.btAdd)
+
+        addButton.setOnClickListener {
+            typeOfOperation = TypeOfOperation.ADD
+            txtViewOperation.text = "+"
+        }
+
+        val subButton = findViewById<Button>(R.id.btSub)
+
+        subButton.setOnClickListener {
+            typeOfOperation = TypeOfOperation.SUB
+            txtViewOperation.text = "-"
+        }
+
+        val multButton = findViewById<Button>(R.id.btMult)
+
+        multButton.setOnClickListener {
+            typeOfOperation = TypeOfOperation.MULT
+            txtViewOperation.text = "x"
+        }
+
+        val divButton = findViewById<Button>(R.id.btDiv)
+
+        divButton.setOnClickListener {
+            typeOfOperation = TypeOfOperation.DIV
+            txtViewOperation.text = "/"
+        }
+
+        val modButton = findViewById<Button>(R.id.btMod)
+
+        modButton.setOnClickListener {
+            typeOfOperation = TypeOfOperation.MODULO
+            txtViewOperation.text = "%"
+        }
+
+        val clearButton = findViewById<Button>(R.id.btClear)
+
+        clearButton.setOnClickListener {
+            typeOfOperation = TypeOfOperation.NONE
+            txtViewOperation.text = ""
+            a=0.0
+            b=0.0
+
+            val inputA: EditText = findViewById(R.id.inputA)
+            inputA.text.clear()
+
+            val inputB: EditText = findViewById(R.id.inputB)
+            inputB.text.clear()
+
+            val txtViewResult: TextView = findViewById(R.id.textViewResult)
+            txtViewResult.text = ""
+
+        }
+
+        val calculateButton = findViewById<Button>(R.id.btCalculate)
+
+        calculateButton.setOnClickListener {
+            calculate()
+        }
+    }
+
+
+
+    fun calculate() {
+
+        val inputA: EditText = findViewById(R.id.inputA)
+        a = inputA.text.toString().toDouble()
+
+        val inputB: EditText = findViewById(R.id.inputB)
+        b= inputB.text.toString().toDouble()
+
         val textViewResult: TextView = findViewById(R.id.textViewResult)
-        textViewResult.setText("he")
-    }
-    fun add(view: android.view.View) {
-        typeOfOperation = TypeOfOperation.ADD
+        if(typeOfOperation == TypeOfOperation.NONE) {
+            textViewResult.setText("Choose type of operation! (+, - etc.")
+            return
+        }
+        if(b == 0.0 && typeOfOperation == TypeOfOperation.DIV) {
+            textViewResult.setText("You can't divide by zero!!")
+            return
+        }
 
-        setContentView(R.layout.activity_main)
-        val tv1: TextView = findViewById(R.id.txtViewTypeOfOperation)
-        tv1.text = "+"
-    }
-    fun sub(view: android.view.View) {
-        typeOfOperation = TypeOfOperation.SUB
+        var result: String = ""
+        result += a
+        var resultNumber = 0.0
+        when(typeOfOperation) {
+            TypeOfOperation.ADD ->  {resultNumber = a+b; result += " + "}
+            TypeOfOperation.SUB -> {resultNumber = a-b; result += " - "}
+            TypeOfOperation.DIV -> {resultNumber = a/b; result += " / "}
+            TypeOfOperation.MULT -> {resultNumber = a*b; result += " x "}
+            TypeOfOperation.MODULO -> {resultNumber = a%b; result += " % "}
+            else -> System.err.println("unspecified operation")
+        }
+        result += b
+        result += " = "
+        result += resultNumber
 
-        setContentView(R.layout.activity_main)
-        val tv1: TextView = findViewById(R.id.txtViewTypeOfOperation)
-        tv1.text = "-"
-    }
-    fun div(view: android.view.View) {
-        typeOfOperation = TypeOfOperation.DIV
-
-        setContentView(R.layout.activity_main)
-        val tv1: TextView = findViewById(R.id.txtViewTypeOfOperation)
-        tv1.text = "/"
-    }
-    fun mult(view: android.view.View) {
-        typeOfOperation = TypeOfOperation.MULT
-
-        setContentView(R.layout.activity_main)
-        val tv1: TextView = findViewById(R.id.txtViewTypeOfOperation)
-        tv1.text = "x"
-    }
-    fun mod(view: android.view.View) {
-        typeOfOperation = TypeOfOperation.MODULO
-
-        setContentView(R.layout.activity_main)
-        val tv1: TextView = findViewById(R.id.txtViewTypeOfOperation)
-        tv1.text = "%"
+        textViewResult.text = result
     }
 
-    fun clear(view: android.view.View) {
-//        setContentView(R.layout.activity_main)
-//        val tvOp: TextView = findViewById(R.id.textViewOperation)
-//        tvOp.text = ""
-//
-//        val editTextA: EditText = findViewById(R.id.inputA)
-//        editTextA.setText("")
-    }
-
-    fun submitA(view: android.view.View) {
-        setContentView(R.layout.activity_main)
-
-        val EditTextinputA: EditText = findViewById(R.id.inputA)
-        val string: String = EditTextinputA.text.toString()
-        //a = Integer.parseInt(string)
-    }
-
-    fun submitB(view: android.view.View) {
-        setContentView(R.layout.activity_main)
-    }
 
 }
