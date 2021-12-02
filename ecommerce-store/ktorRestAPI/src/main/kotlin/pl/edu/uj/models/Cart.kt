0060ca51d6b23a588e8fff2ca703d4a2d0d7e3fc
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-02 18:07:25 +0100

    added cart CRUD functions and routing

--- ecommerce-store/ktorRestAPI/src/main/kotlin/pl/edu/uj/models/Cart.kt
+++ ecommerce-store/ktorRestAPI/src/main/kotlin/pl/edu/uj/models/Cart.kt
@@ -1,13 +1,15 @@
 package pl.edu.uj.models
 
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
 data class Cart(val customerId : Int, val productId : Int, var quantity : Int)
 
 object CartTable : Table() {
     val customerId = integer("customerId").references(CustomerTable.id)
     val productId = integer("productId").references(ProductTable.id)
+    override val primaryKey = PrimaryKey(customerId, productId)
+
     var quantity = integer("quantity")
 }
 
@@ -17,4 +19,48 @@ fun ResultRow.toCart() = Cart(
     quantity = this[CartTable.quantity]
 )
 
-// TODO functions
+fun getAllCarts() {
+    transaction {
+        CartTable.selectAll()
+    }
+}
+
+fun getCustomerCart(customerId : Int) {
+    transaction {
+        CartTable.select { CartTable.customerId eq customerId }.map { it.toCart() }
+    }
+}
+
+fun deleteFromCart(customerId: Int, productId: Int) {
+    transaction {
+        CartTable.deleteWhere { CartTable.customerId eq customerId and (CartTable.productId eq productId) }
+    }
+}
+
+fun addToCart(customerId: Int, productId: Int) {
+    transaction {
+        val count = CartTable.select { CartTable.customerId eq  customerId and (CartTable.productId eq productId) }.count()
+
+        // product already in cart, increase quantity
+        if(count > 0) {
+            CartTable.update({ (CartTable.customerId eq customerId) and (CartTable.productId eq productId) }) {
+                with(SqlExpressionBuilder) {
+                    it[quantity] = quantity + 1
+                }
+            }
+        } else {
+            CartTable.insert {
+                it[CartTable.customerId] = customerId
+                it[CartTable.productId] = productId
+                it[quantity] = 0
+
+            }
+        }
+    }
+}
+
+fun deleteAllCarts() {
+    transaction {
+        CartTable.deleteAll()
+    }
+}
--- ecommerce-store/ktorRestAPI/src/main/kotlin/pl/edu/uj/routes/CartRoutes.kt
+++ ecommerce-store/ktorRestAPI/src/main/kotlin/pl/edu/uj/routes/CartRoutes.kt
@@ -3,37 +3,47 @@ package pl.edu.uj.routes
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
+import pl.edu.uj.models.*
 
 fun Route.cartRouting() {
     route("/cart") {
-        // gets all products from cart
+
+        // gets all products from cart from every customer
         get {
-//           call.respond(
-//            transaction {
-//                testTable.selectAll()
-//            })
+            call.respond(getAllCarts())
         }
 
-        // puts product in cart
-        post("{id}") {
-
+        // gets customer's cart
+        get("/{customer_id}") {
+            val customerId = call.parameters["customer_id"]
+            if(customerId != null) {
+                call.respond(getCustomerCart(customerId.toInt()))
+            }
         }
 
-        // deteles all products from cart
+
+        // puts product in customer's cart
+        post("/{customer_id}/{product_id}") {
+            val customerId = call.parameters["customer_id"]
+            val productId = call.parameters["product_id"]
+            if(customerId != null && productId != null) {
+                call.respond(addToCart(customerId.toInt(), productId.toInt()))
+            }
+        }
+
+        // deteles all products from cart from all customers
         delete {
-
+            call.respond(deleteAllCarts())
         }
 
         // deletes a product from cart
-        delete("{id}") {
-
+        delete("/{customer_id}/{product_id}") {
+            val customerId = call.parameters["customer_id"]
+            val productId = call.parameters["product_id"]
+            if(customerId != null && productId != null) {
+                call.respond(deleteFromCart(customerId.toInt(), productId.toInt()))
+            }
         }
-
-//        put {
-//
-//        }
 
 
     }
