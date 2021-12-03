5d6d9195c88ef2038c40760b27012ad4147b61d8
Stepi <37037163+Stempnio@users.noreply.github.com>
2021-12-03 09:22:15 +0100

    deleted old Ktor rest api directory

--- ecommerce-store/ktor/src/models/Customer.kt
+++
@@ -1,5 +0,0 @@
-package pl.edu.uj.models
-
-data class Customer(val id : String, val firstName : String, val lastName : String, val email : String)
-
-val customerStorage = mutableListOf<Customer>()
