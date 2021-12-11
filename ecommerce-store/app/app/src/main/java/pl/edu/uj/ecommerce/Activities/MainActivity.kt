3b62d1566109638c49494d1b700e0b58195f0adf
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-11 12:25:29 +0100

    added fragments and corresponding layouts

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Activities/MainActivity.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Activities/MainActivity.kt
@@ -1,18 +1,28 @@
 package pl.edu.uj.ecommerce.Activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.edu.uj.ecommerce.Fragments.ProductsFragment
 import pl.edu.uj.ecommerce.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val productsFragment = ProductsFragment()
 
-        val intent = Intent(this, LogInActivity::class.java).apply {
-//            putExtra("message",labelka.text)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frContainerViewActivityMain, productsFragment)
+            setReorderingAllowed(true)
+            commit()
         }
-        startActivity(intent)
+
+
+
+
+//        val intent = Intent(this, LogInActivity::class.java).apply {
+////            putExtra("message",labelka.text)
+//        }
+//        startActivity(intent)
     }
 }
---
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/RegisterFragment.kt
@@ -0,0 +1,12 @@
+package pl.edu.uj.ecommerce.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pl.edu.uj.ecommerce.R
+
+class RegisterFragment : Fragment(R.layout.fragment_products) {
+
+}
