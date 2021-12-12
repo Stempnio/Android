e42973a30492c4c08dab78b5bd15dc4103c40630
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-12 22:10:30 +0100

    working recycler view for products

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/ProductsFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/ProductsFragment.kt
@@ -5,8 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import pl.edu.uj.ecommerce.Adapters.ProductTmpAdapter
+import pl.edu.uj.ecommerce.Products
 import pl.edu.uj.ecommerce.R
 
-class ProductsFragment : Fragment(R.layout.fragment_products) {
+class ProductsFragment : Fragment() {
 
+    lateinit var button: Button
+    lateinit var textView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_products, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = LinearLayoutManager(context)
+
+//                // TODO delete??
+//                Products.getProductsFromDB()
+
+                adapter = ProductTmpAdapter()
+            }
+        }
+        return view
+    }
 }
