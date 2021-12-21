6be518aa1b2493c54e00f34908c48b99924a7b90
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-21 18:05:55 +0100

    register fragment working go back button

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/RegisterFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/RegisterFragment.kt
@@ -5,8 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import pl.edu.uj.ecommerce.R
+import pl.edu.uj.ecommerce.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment(R.layout.fragment_products) {
+    var _binding : FragmentRegisterBinding? = null
+    val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        binding.btnRegisterGoBack.setOnClickListener {
+            findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLogInFragment())
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
