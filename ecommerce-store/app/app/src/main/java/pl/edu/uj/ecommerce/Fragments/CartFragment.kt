a142d1e74cc6d21491a395c3c9ff6f13aa0472d1
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-21 18:03:02 +0100

    cart recycler view

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/CartFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/CartFragment.kt
@@ -1,15 +1,21 @@
 package pl.edu.uj.ecommerce.Fragments
 
+import RecyclerViewCartAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+//import pl.edu.uj.ecommerce.Adapters.CartListAdapter
+import pl.edu.uj.ecommerce.Data.CURRENT_CUSTOMER_ID
+import pl.edu.uj.ecommerce.Data.CartItemRealm
 import pl.edu.uj.ecommerce.R
 import pl.edu.uj.ecommerce.databinding.FragmentCartBinding
 
-class CartFragment : Fragment(R.layout.fragment_products) {
+class CartFragment : Fragment(R.layout.fragment_cart) {
     private var _binding: FragmentCartBinding? = null
     private val binding get() = _binding!!
 
@@ -19,12 +25,15 @@ class CartFragment : Fragment(R.layout.fragment_products) {
     ): View {
         _binding = FragmentCartBinding.inflate(layoutInflater, container, false)
 
-        //TODO recyclerview cart
-//        val recyclerViewCartList = binding.RecyclerViewCartList
-//        recyclerViewCartList.layoutManager = LinearLayoutManager(context)
-//
-//        val products = Products.getProductsFromDbIntoList()
-//        recyclerViewCartList.adapter = ProductCartAdapter(products)
+        val recyclerViewCartList = binding.RecyclerViewCartList
+        recyclerViewCartList.layoutManager = LinearLayoutManager(context)
+
+        val adapter = RecyclerViewCartAdapter(Realm.getDefaultInstance()
+            .where(CartItemRealm::class.java)
+            .equalTo("customerId", CURRENT_CUSTOMER_ID)
+            .findAll())
+
+        recyclerViewCartList.adapter = adapter
 
 
         binding.buttonBuy.setOnClickListener {
