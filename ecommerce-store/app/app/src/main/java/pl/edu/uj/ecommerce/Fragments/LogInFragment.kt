92158a38907e6e9a2554c54344c7ce6180409816
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-21 18:04:07 +0100

    basic log in system (requiring only customer id)

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/LogInFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/LogInFragment.kt
@@ -1,14 +1,23 @@
 package pl.edu.uj.ecommerce.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import pl.edu.uj.ecommerce.Data.CURRENT_CUSTOMER_ID
+import pl.edu.uj.ecommerce.Data.Customer
+import pl.edu.uj.ecommerce.Data.getCartIntoDB
+import pl.edu.uj.ecommerce.Data.getCustomerByIdIntoDB
 import pl.edu.uj.ecommerce.R
+import pl.edu.uj.ecommerce.RetrofitService
 import pl.edu.uj.ecommerce.databinding.FragmentLogInBinding
-import pl.edu.uj.ecommerce.databinding.FragmentTestBinding
+import pl.edu.uj.ecommerce.getProductsIntoDB
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LogInFragment : Fragment(R.layout.fragment_products) {
 
@@ -22,8 +31,17 @@ class LogInFragment : Fragment(R.layout.fragment_products) {
     ): View {
         _binding = FragmentLogInBinding.inflate(layoutInflater, container, false)
 
+        binding.buttonSignUp.setOnClickListener {
+            findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToRegisterFragment())
+        }
+
         binding.buttonLogIn.setOnClickListener {
-            findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToProductsFragment())
+            // TODO logging system (for example oauth)
+
+            val id = binding.editTextTextEmail.text.toString()
+            var password = binding.editTextPassword.text.toString()
+
+            logIn(id)
         }
 
         return binding.root
@@ -32,5 +50,34 @@ class LogInFragment : Fragment(R.layout.fragment_products) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun logIn(id : String) {
+        val service = RetrofitService.create()
+        val call = service.getCustomerByIdCall(id)
+
+        call.enqueue(object : Callback<Customer> {
+            override fun onResponse(
+                call: Call<Customer>,
+                response: Response<Customer>
+            ) {
+                if (response.code() == 200) {
+                    CURRENT_CUSTOMER_ID = id
+
+                    // update products and customers cart
+                    getProductsIntoDB()
+                    getCustomerByIdIntoDB(CURRENT_CUSTOMER_ID)
+                    getCartIntoDB(CURRENT_CUSTOMER_ID)
+
+                    findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToProductsFragment())
+                }
+
+            }
+
+            override fun onFailure(call: Call<Customer>, t: Throwable) {
+                Toast.makeText(context, "LOG IN FAILED", Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
