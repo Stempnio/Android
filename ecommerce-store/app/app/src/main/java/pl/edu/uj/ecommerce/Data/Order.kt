c31bb8d408067b9edae7208ca039c7bb69110a8b
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-22 15:47:17 +0100

    refreshing cart after order

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Data/Order.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Data/Order.kt
@@ -1,21 +1,16 @@
 package pl.edu.uj.ecommerce.Data
 import android.util.Log
-import android.widget.Toast
 import io.realm.Realm
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-import pl.edu.uj.ecommerce.Products
 import pl.edu.uj.ecommerce.RetrofitService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.time.LocalDateTime
-import java.util.*
 
 open class OrderRealm : RealmObject() {
     var customerId : String = ""
-    //TODO date format
-//    var date : Date = Date()
     var date : String = LocalDateTime.MIN.toString()
     @PrimaryKey
     var id : Int = -1
@@ -24,30 +19,17 @@ open class OrderRealm : RealmObject() {
 
 class Order {
     var customerId : String = ""
-//    var date : Date = Date()
     var date : String = LocalDateTime.MIN.toString()
     var id : Int = -1
 }
 
-//fun ordersToString() : String {
-//
-//    var result = ""
-//
-//    Realm.getDefaultInstance()
-//        .where(OrderRealm::class.java)
-//        .equalTo("customerId", CURRENT_CUSTOMER_ID)
-//        .findAll().forEach {
-//            result += Products.productDetails(it.id)
-//        }
-//
-//    return result
-//}
 
 fun postOrder() {
     val service = RetrofitService.create()
     val call = service.postCustomerOrderCall(CURRENT_CUSTOMER_ID)
     call.enqueue(object : Callback<Order> {
         override fun onResponse(call: Call<Order>, response: Response<Order>) {
+
             Log.d("POST ORDER SUCCESSFUL", response.message())
         }
 
@@ -56,6 +38,8 @@ fun postOrder() {
         }
 
     })
+
+    refreshCart()
 }
 
 fun getOrdersIntoDB() {
