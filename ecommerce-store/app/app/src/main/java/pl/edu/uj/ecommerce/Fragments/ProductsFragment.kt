c721f57747f8f84040d07b073c4e921b32f593b4
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-22 12:49:47 +0100

    added options menu, about app fragment only accessible from products fragment

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/ProductsFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/ProductsFragment.kt
@@ -1,10 +1,8 @@
 package pl.edu.uj.ecommerce.Fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import pl.edu.uj.ecommerce.Adapters.ProductListAdapter
@@ -12,6 +10,7 @@ import pl.edu.uj.ecommerce.Data.CURRENT_CUSTOMER_ID
 import pl.edu.uj.ecommerce.Data.DEFAULT_CUSTOMER_ID
 import pl.edu.uj.ecommerce.Data.getCartIntoDB
 import pl.edu.uj.ecommerce.Products.getProductsFromDbIntoList
+import pl.edu.uj.ecommerce.R
 import pl.edu.uj.ecommerce.databinding.FragmentProductsBinding
 
 class ProductsFragment : Fragment() {
@@ -32,9 +31,6 @@ class ProductsFragment : Fragment() {
         recyclerViewProductList.adapter = ProductListAdapter(products)
 
 
-        binding.buttonAboutApp.setOnClickListener {
-            findNavController().navigate(ProductsFragmentDirections.actionProductsFragmentToAboutAppFragment())
-        }
 
         binding.buttonGoToCart.setOnClickListener {
             getCartIntoDB(CURRENT_CUSTOMER_ID)
@@ -50,11 +46,31 @@ class ProductsFragment : Fragment() {
             findNavController().navigate(ProductsFragmentDirections.actionProductsFragmentToOrdersFragment())
         }
 
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.aboutAppFragment){
+            findNavController().navigate(ProductsFragmentDirections.actionProductsFragmentToAboutAppFragment())
+            return true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
 }
