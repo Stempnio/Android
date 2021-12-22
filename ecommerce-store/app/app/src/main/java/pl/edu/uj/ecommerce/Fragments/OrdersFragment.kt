c422404ba49c85bdc67769a17ae3bd0573d4b8ba
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-22 09:44:51 +0100

    added order fragment

---
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/OrdersFragment.kt
@@ -0,0 +1,44 @@
+package pl.edu.uj.ecommerce.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import io.realm.Realm
+import pl.edu.uj.ecommerce.Data.CURRENT_CUSTOMER_ID
+import pl.edu.uj.ecommerce.Data.OrderRealm
+import pl.edu.uj.ecommerce.R
+import pl.edu.uj.ecommerce.databinding.FragmentOrdersBinding
+
+
+class OrdersFragment : Fragment(R.layout.fragment_orders) {
+    private var _binding: FragmentOrdersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOrdersBinding.inflate(layoutInflater, container, false)
+
+        binding.tvOrdersFragmentOrders.text = Realm.getDefaultInstance()
+            .where(OrderRealm::class.java)
+            .equalTo("customerId", CURRENT_CUSTOMER_ID)
+            .findAll()
+            .toString()
+
+        binding.btnOrdersGoBack.setOnClickListener {
+            findNavController().navigate(OrdersFragmentDirections.actionOrdersFragmentToProductsFragment())
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
