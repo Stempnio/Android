fa99efc8592d7c49a9df2ef3031dd73b6b9f770e
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-22 14:10:52 +0100

    added post order function

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Data/Order.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Data/Order.kt
@@ -1,8 +1,10 @@
 package pl.edu.uj.ecommerce.Data
 import android.util.Log
+import android.widget.Toast
 import io.realm.Realm
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import pl.edu.uj.ecommerce.Products
 import pl.edu.uj.ecommerce.RetrofitService
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,7 +16,7 @@ open class OrderRealm : RealmObject() {
     var customerId : String = ""
     //TODO date format
 //    var date : Date = Date()
-    var date : String = ""
+    var date : String = LocalDateTime.MIN.toString()
     @PrimaryKey
     var id : Int = -1
 
@@ -23,14 +25,42 @@ open class OrderRealm : RealmObject() {
 class Order {
     var customerId : String = ""
 //    var date : Date = Date()
-    var date : String = ""
+    var date : String = LocalDateTime.MIN.toString()
     var id : Int = -1
 }
 
+//fun ordersToString() : String {
+//
+//    var result = ""
+//
+//    Realm.getDefaultInstance()
+//        .where(OrderRealm::class.java)
+//        .equalTo("customerId", CURRENT_CUSTOMER_ID)
+//        .findAll().forEach {
+//            result += Products.productDetails(it.id)
+//        }
+//
+//    return result
+//}
 
-fun getOrdersIntoDB(customerId : String) {
+fun postOrder() {
     val service = RetrofitService.create()
-    val call = service.getCustomerOrdersCall(customerId)
+    val call = service.postCustomerOrderCall(CURRENT_CUSTOMER_ID)
+    call.enqueue(object : Callback<Order> {
+        override fun onResponse(call: Call<Order>, response: Response<Order>) {
+            Log.d("POST ORDER SUCCESSFUL", response.message())
+        }
+
+        override fun onFailure(call: Call<Order>, t: Throwable) {
+            Log.d("POST ORDER FAIL", t.message.toString())
+        }
+
+    })
+}
+
+fun getOrdersIntoDB() {
+    val service = RetrofitService.create()
+    val call = service.getCustomerOrdersCall(CURRENT_CUSTOMER_ID)
     call.enqueue(object : Callback<List<Order>> {
         override fun onResponse(
             call: Call<List<Order>>,
@@ -42,7 +72,7 @@ fun getOrdersIntoDB(customerId : String) {
                 for (item in orderResponse) {
                     val tmpItem = OrderRealm().apply {
                         this.customerId = item.customerId
-                        this.date = item.date.toString()
+                        this.date = item.date
                         this.id = item.id
                     }
 
