77eae2f9e8d2f3f856d93b1bed3c4a2df534950c
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-22 09:45:57 +0100

    added retrofit functions

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/RetrofitService.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/RetrofitService.kt
@@ -2,6 +2,8 @@ package pl.edu.uj.ecommerce
 
 import pl.edu.uj.ecommerce.Data.CartItem
 import pl.edu.uj.ecommerce.Data.Customer
+import pl.edu.uj.ecommerce.Data.Order
+import pl.edu.uj.ecommerce.Data.OrderDetails
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,16 +11,21 @@ import retrofit2.http.*
 
 interface RetrofitService {
 
+//    Products
     @GET("product")
     fun getProductsCall() : Call<List<Product>>
 
+//    Customer
     @GET("customer/{id}")
     fun getCustomerByIdCall(@Path("id") id : String) : Call<Customer>
 
-    //TODO customer registration
     @POST("customer")
     fun postCustomerCall(@Body customer : Customer) : Call<Customer>
 
+    @DELETE("customer/{customerId}")
+    fun deleteCustomerCall(@Path("customerId") customerId: String) : Call<Customer>
+
+//    Cart
     @GET("cart/{customerId}")
     fun getCartByIdCall(@Path("customerId") customerId : String) : Call<List<CartItem>>
 
@@ -28,12 +35,28 @@ interface RetrofitService {
     @DELETE("cart/{customerId}/{productId}")
     fun deleteCartItemCall(@Path("customerId") customerId: String, @Path("productId") productId: Int) : Call<CartItem>
 
-    //TODO order
-    //TODO orderdetails
+    @DELETE("cart/{customerId}")
+    fun deleteCartCall(@Path("customerId") customerId: String) : Call<List<CartItem>>
+
+//    Orders
+    @GET("order/customer/{customerId}")
+    fun getCustomerOrdersCall(@Path("customerId") customerId: String) : Call<List<Order>>
+
+    @POST("order/{customerId}")
+    fun postCustomerOrderCall(@Path("customerId") customerId: String) : Call<Order>
+
+//    Order details
+    @GET("oderDetails/{orderId}")
+    fun getOrderDetailsByIdCall(@Path("orderId") orderId : Int) : Call<List<OrderDetails>>
+
+    @GET("orderDetails/customer/{customerId}")
+    fun getCustomerOrderDetailsCall(@Path("customerId") customerId: String) : Call<List<OrderDetails>>
+
 
     companion object {
 
-        var BASE_URL = "https://7fe9-185-25-121-195.ngrok.io/"
+//        var BASE_URL = "https://1bae-185-25-121-195.ngrok.io/"
+        var BASE_URL = "http://10.0.2.2:8080/"
 
         fun create() : RetrofitService {
 
