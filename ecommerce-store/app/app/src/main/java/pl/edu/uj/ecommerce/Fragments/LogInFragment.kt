e11dfc84e497b4de8e9bdacaeb5d764d90f8bb4e
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-22 09:48:55 +0100

    get orders and orderdetails into database after log in

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/LogInFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/LogInFragment.kt
@@ -7,10 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import pl.edu.uj.ecommerce.Data.CURRENT_CUSTOMER_ID
-import pl.edu.uj.ecommerce.Data.Customer
-import pl.edu.uj.ecommerce.Data.getCartIntoDB
-import pl.edu.uj.ecommerce.Data.getCustomerByIdIntoDB
+import pl.edu.uj.ecommerce.Data.*
 import pl.edu.uj.ecommerce.R
 import pl.edu.uj.ecommerce.RetrofitService
 import pl.edu.uj.ecommerce.databinding.FragmentLogInBinding
@@ -68,6 +65,8 @@ class LogInFragment : Fragment(R.layout.fragment_products) {
                     getProductsIntoDB()
                     getCustomerByIdIntoDB(CURRENT_CUSTOMER_ID)
                     getCartIntoDB(CURRENT_CUSTOMER_ID)
+                    getOrdersIntoDB(CURRENT_CUSTOMER_ID)
+                    getOrderDetailsIntoDB(CURRENT_CUSTOMER_ID)
 
                     findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToProductsFragment())
                 }
