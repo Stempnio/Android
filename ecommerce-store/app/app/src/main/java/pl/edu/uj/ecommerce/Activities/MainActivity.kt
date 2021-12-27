ac0f76e7dad29cac132426cc4a7badb14263e5e2
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-27 13:05:43 +0100

    install splash screen added in main activity

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Activities/MainActivity.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Activities/MainActivity.kt
@@ -2,6 +2,7 @@ package pl.edu.uj.ecommerce.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import pl.edu.uj.ecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
