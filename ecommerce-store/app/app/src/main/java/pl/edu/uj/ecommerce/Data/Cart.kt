006cfaf12800f7b2f91206f3570fd93d8ae448ff
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-06 12:47:43 +0100

    admin cart functions and layout

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Data/Cart.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Data/Cart.kt
@@ -4,8 +4,6 @@ import android.util.Log
 import io.realm.Realm
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-import pl.edu.uj.ecommerce.ProductRealm
-import pl.edu.uj.ecommerce.Products
 import pl.edu.uj.ecommerce.RetrofitService
 import retrofit2.Call
 import retrofit2.Callback
@@ -153,6 +151,37 @@ fun getCartIntoDB() {
 
         override fun onFailure(call: Call<List<CartItem>>, t: Throwable) {
             Log.d("GET_CART_FROM_DB", t.message.toString())
+        }
+
+    })
+}
+
+fun deleteCustomerCart(customerId : String) {
+    val service = RetrofitService.create()
+    val call = service.deleteCustomerCartCall(customerId)
+    call.enqueue(object : Callback<List<CartItem>> {
+        override fun onResponse(call: Call<List<CartItem>>, response: Response<List<CartItem>>) {
+            if(response.isSuccessful)
+                Log.d("DELETE_CUSTOMER_CART", "success")
+        }
+
+        override fun onFailure(call: Call<List<CartItem>>, t: Throwable) {
+            Log.d("DELETE_CUSTOMER_CART", t.message.toString())
+        }
+
+    })
+}
+
+fun deleteAllCarts() {
+    val service = RetrofitService.create()
+    val call = service.deleteAllCarts()
+    call.enqueue(object : Callback<List<CartItem>> {
+        override fun onResponse(call: Call<List<CartItem>>, response: Response<List<CartItem>>) {
+            Log.d("DELETE_ALL_CARTS", "success")
+        }
+
+        override fun onFailure(call: Call<List<CartItem>>, t: Throwable) {
+            Log.d("DELETE_ALL_CARTS", t.message.toString())
         }
 
     })
--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/admin/AdminCartFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/admin/AdminCartFragment.kt
@@ -1,13 +1,21 @@
 package pl.edu.uj.ecommerce.admin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import pl.edu.uj.ecommerce.Data.CartItem
+import pl.edu.uj.ecommerce.Data.Product
+import pl.edu.uj.ecommerce.Data.deleteAllCarts
+import pl.edu.uj.ecommerce.Data.deleteCustomerCart
 import pl.edu.uj.ecommerce.R
+import pl.edu.uj.ecommerce.RetrofitService
 import pl.edu.uj.ecommerce.databinding.FragmentAboutAppBinding
 import pl.edu.uj.ecommerce.databinding.FragmentAdminCartBinding
+import retrofit2.Call
+import retrofit2.Response
 
 
 class AdminCartFragment : Fragment() {
@@ -22,9 +30,57 @@ class AdminCartFragment : Fragment() {
     ): View {
         _binding = FragmentAdminCartBinding.inflate(layoutInflater, container, false)
 
+        binding.btnAdminCartDeleteAll.setOnClickListener {
+            deleteAllCarts()
+        }
+
+        binding.btnAdminCartDeleteCustomerCart.setOnClickListener {
+            val id = binding.etAdminCartDeleteCustomerCart.text.toString()
+            deleteCustomerCart(id)
+        }
+
+        binding.btnAdminCartRefresh.setOnClickListener {
+            getAllCarts()
+        }
+
         return binding.root
     }
 
+    private fun getAllCarts() {
+        val service = RetrofitService.create()
+        val call = service.getAllCartItemsCall()
+        call.enqueue(object : retrofit2.Callback<List<CartItem>> {
+            override fun onResponse(
+                call: Call<List<CartItem>>,
+                response: Response<List<CartItem>>
+            ) {
+                val cartList = response.body()
+
+                if(cartList != null) {
+                    binding.tvAdminCartList.text = cartToString(cartList)
+                } else {
+                    binding.tvAdminCartList.text = "not found"
+                }
+
+                Log.d("GET_ALL_CARTS", "success")
+            }
+
+            override fun onFailure(call: Call<List<CartItem>>, t: Throwable) {
+                binding.tvAdminCartList.text = t.message.toString()
+            }
+
+
+        })
+    }
+
+    fun cartToString( list : List<CartItem>) : String {
+        var result = ""
+        list.forEach { result += "customerID:" +
+                it.customerId + "| product:" +
+                it.productId + "| quantity:" +
+                it.quantity + "\n" }
+        return result
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
