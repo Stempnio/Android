153d61b72859e3a34cbffd0c35d8e8d35a723fe1
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-08 14:34:23 +0100

    added automated cart tests

---
+++ ecommerce-store/ktorRestAPI/src/test/kotlin/pl/edu/uj/CartTest.kt
@@ -0,0 +1,70 @@
+package pl.edu.uj
+
+import com.google.gson.Gson
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Before
+import pl.edu.uj.models.Cart
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CartTest {
+
+    val customerTest = CustomerTest()
+    val customer = customerTest.customer
+
+    val productTest = ProductTest()
+    val product = productTest.product1
+
+
+    private val cart = mutableListOf(Cart(customer.id, product.id, 1))
+    private val emptyCart = mutableListOf<Cart>()
+
+    @Before
+    fun setup() {
+        customerTest.testPostCustomer()
+        productTest.testPostAndGetProduct()
+    }
+
+    @Test
+    fun testPostAndGetCartItem() {
+        withTestApplication({ module(testing = true) }) {
+            with(handleRequest(HttpMethod.Post, "/cart/${customer.id}/${product.id}")) {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+
+            handleRequest(HttpMethod.Get, "/cart/${customer.id}").apply {
+                assertEquals(Gson().toJson(cart), response.content)
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+        }
+    }
+
+    @Test
+    fun testDeleteCartItem() {
+        withTestApplication({ module(testing = true) }) {
+
+            testPostAndGetCartItem()
+
+            handleRequest(HttpMethod.Delete, "/cart/${customer.id}/${product.id}")
+
+            handleRequest(HttpMethod.Get, "/cart/${customer.id}").apply {
+                assertEquals(Gson().toJson(emptyCart), response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testDeleteAllCarts() {
+        withTestApplication({ module(testing = true) }) {
+
+            testPostAndGetCartItem()
+
+            handleRequest(HttpMethod.Delete, "/cart")
+
+            handleRequest(HttpMethod.Get, "/cart").apply {
+                assertEquals(Gson().toJson(emptyCart), response.content)
+            }
+        }
+    }
+}
