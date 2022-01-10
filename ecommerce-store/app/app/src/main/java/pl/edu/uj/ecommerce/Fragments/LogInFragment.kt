3b3aeb62ed453ec1162dce43bb666f25a8fcb586
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-10 22:29:40 +0100

    added sign in with google

--- ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/LogInFragment.kt
+++ ecommerce-store/app/app/src/main/java/pl/edu/uj/ecommerce/Fragments/LogInFragment.kt
@@ -1,5 +1,7 @@
 package pl.edu.uj.ecommerce.Fragments
 
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,21 +10,30 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+
 import pl.edu.uj.ecommerce.Data.*
 import pl.edu.uj.ecommerce.R
 import pl.edu.uj.ecommerce.RetrofitService
-import pl.edu.uj.ecommerce.databinding.FragmentLogInBinding
-import pl.edu.uj.ecommerce.Data.getProductsIntoDB
 import pl.edu.uj.ecommerce.admin.Admin
+import pl.edu.uj.ecommerce.databinding.FragmentLogInBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class LogInFragment : Fragment(R.layout.fragment_products) {
 
     private var _binding : FragmentLogInBinding? = null
-
     private val binding get() = _binding!!
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,24 +41,130 @@ class LogInFragment : Fragment(R.layout.fragment_products) {
     ): View {
         _binding = FragmentLogInBinding.inflate(layoutInflater, container, false)
 
+        setUpGoogleClient()
+
         binding.buttonSignUp.setOnClickListener {
             findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToRegisterFragment())
         }
 
         binding.buttonLogIn.setOnClickListener {
-            // TODO oauth2
-
             val id = binding.editTextUsername.text.toString()
             val password = binding.editTextPassword.text.toString()
             logIn(id, password)
         }
 
+        binding.buttonLogInWithGoogle.setOnClickListener {
+            googleLogIn()
+        }
+
         return binding.root
     }
+
+
+    private fun setUpGoogleClient() {
+        oneTapClient = Identity.getSignInClient(requireActivity())
+        oneTapClient.signOut()
+
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.serverId))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build()
+    }
+
+    private fun googleLogIn() {
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(requireActivity()) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e("TAG", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(requireActivity()) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                Log.d("TAG", e.localizedMessage)
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val googleCustomer = Customer().apply { this.id =
+                        if(credential.id.length <= 20)
+                            credential.id
+                        else
+                            credential.id.substring(0,19)}
+                    googlePostCustomerIfNotExists(googleCustomer)
+
+
+                } catch (e: ApiException) {
+                }
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun logIn(id : String, password: String) {
+
+        if(id == "" || password == "") {
+            Toast.makeText(context, "ENTER USERNAME AND PASSWORD", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if(id.contains("admin")) {
+            adminLogIn(id, password)
+        } else {
+            customerLogIn(id, password)
+        }
+
+    }
+
+    private fun googlePostCustomerIfNotExists(customer: Customer) {
+        val service = RetrofitService.create()
+        val call = service.getCustomerByIdCall(customer.id)
+        call.enqueue(object : Callback<Customer> {
+            override fun onResponse(
+                call: Call<Customer>,
+                response: Response<Customer>
+            ) {
+                if (response.code() != 200) {
+                    postCustomer(customer)
+                }
+
+                CURRENT_CUSTOMER_ID = customer.id
+
+                findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToProductsFragment())
+                // update products and customers cart
+                getProductsIntoDB()
+                getCustomerByIdIntoDB(CURRENT_CUSTOMER_ID)
+
+                refreshCart()
+                refreshOrders()
+
+            }
+
+            override fun onFailure(call: Call<Customer>, t: Throwable) {
+                Log.d("GET CUSTOMER INTO DB FAILED", t.message.toString())
+            }
+
+        })
     }
 
     private fun customerLogIn(id : String, password: String) {
@@ -119,19 +236,5 @@ class LogInFragment : Fragment(R.layout.fragment_products) {
         })
     }
 
-    fun logIn(id : String, password: String) {
 
-        if(id == "" || password == "") {
-            Toast.makeText(context, "ENTER USERNAME AND PASSWORD", Toast.LENGTH_LONG).show()
-            return
-        }
-
-        if(id.contains("admin")) {
-            adminLogIn(id, password)
-        } else {
-            customerLogIn(id, password)
-        }
-
-
-    }
 }
