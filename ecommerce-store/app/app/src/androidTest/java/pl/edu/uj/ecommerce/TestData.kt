f2053ce688890695c58765593978f189416624e6
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-11 11:49:02 +0100

    created TestData.kt containing data and functions for tests

---
+++ ecommerce-store/app/app/src/androidTest/java/pl/edu/uj/ecommerce/TestData.kt
@@ -0,0 +1,43 @@
+package pl.edu.uj.ecommerce
+
+import pl.edu.uj.ecommerce.Data.*
+
+val testCustomer = Customer().apply {
+    this.id="testCustomer"
+    this.password = "1234"
+}
+
+val testProduct = Product().apply {
+    this.id = 1
+    this.price = 100
+    this.name = "test product 1"
+}
+
+fun addTestCustomer() {
+    RetrofitService
+        .create()
+        .deleteCustomerCall(testCustomer.id)
+        .execute()
+
+    RetrofitService
+        .create()
+        .postCustomerCall(testCustomer)
+        .execute()
+
+    CURRENT_CUSTOMER_ID = testCustomer.id
+    getCustomerByIdIntoDB(CURRENT_CUSTOMER_ID)
+}
+
+fun addTestProduct() {
+    RetrofitService
+        .create()
+        .deleteAllProductsCall()
+        .execute()
+
+    RetrofitService
+        .create()
+        .postProductCall(testProduct)
+        .execute()
+
+    getProductsIntoDB()
+}
