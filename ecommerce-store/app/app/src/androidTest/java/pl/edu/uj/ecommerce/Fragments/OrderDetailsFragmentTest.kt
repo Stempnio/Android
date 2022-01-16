9f9371c78e08f7d50372efecb611c435851ea801
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-16 14:54:18 +0100

    modified testCorrectOrderDetails

--- ecommerce-store/app/app/src/androidTest/java/pl/edu/uj/ecommerce/Fragments/OrderDetailsFragmentTest.kt
+++ ecommerce-store/app/app/src/androidTest/java/pl/edu/uj/ecommerce/Fragments/OrderDetailsFragmentTest.kt
@@ -11,13 +11,13 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
+import org.hamcrest.core.StringContains.containsString
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import pl.edu.uj.ecommerce.*
 import pl.edu.uj.ecommerce.Data.CURRENT_CUSTOMER_ID
 import pl.edu.uj.ecommerce.Data.OrderDetails
-import pl.edu.uj.ecommerce.Data.orderDetailsToString
 
 @RunWith(AndroidJUnit4::class)
 class OrderDetailsFragmentTest {
@@ -33,6 +33,8 @@ class OrderDetailsFragmentTest {
         addTestCustomer()
         addTestProduct()
         addTestOrder()
+
+        wait(1000)
 
         val orderDetailsList = RetrofitService
             .create()
@@ -60,9 +62,9 @@ class OrderDetailsFragmentTest {
     fun testCorrectOrderDetails() {
         Truth.assertThat(orderDetails).isNotNull()
 
-        val expectedString = orderDetailsToString(orderDetails!!.orderId)
-
-        onView(withId(R.id.tvOrderDetails)).check(matches(withText(expectedString)))
+        onView(withId(R.id.tvOrderDetails)).check(matches(withText(containsString("Product: ${testProduct.name}"))))
+        onView(withId(R.id.tvOrderDetails)).check(matches(withText(containsString("Order ID: ${orderDetails!!.orderId}"))))
+        onView(withId(R.id.tvOrderDetails)).check(matches(withText(containsString("price: ${testProduct.price}"))))
     }
 
     @Test
